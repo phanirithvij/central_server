40752a79017580e8d5ef13e62d1b01090aa7c9f0
phanirithvij <phanirithvij2000@gmail.com>
2020-11-25 16:59:53 +0530

    shutdown grace?

--- server/utils/upload/upload.go
+++ server/utils/upload/upload.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"context"
 	"errors"
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
 	"path/filepath"
 	"time"
 
@@ -72,6 +75,11 @@ func (h spaHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	// https://github.com/gorilla/mux/#graceful-shutdown
+	var wait time.Duration
+	flag.DurationVar(&wait, "graceful-timeout", time.Second*15, "the duration for which the server gracefully wait for existing connections to finish - e.g. 15s or 1m")
+	flag.Parse()
+
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 	os.Mkdir("uploads", os.ModeDir)
 	// Create a new FileStore instance which is responsible for
@@ -131,14 +139,40 @@ func main() {
 	log.Println("Starting on localhost:8080")
 	srv := &http.Server{
 		Handler: r,
-		Addr:    ":8080",
-		// Good practice: enforce timeouts for servers you create!
-		WriteTimeout: 15 * time.Second,
-		ReadTimeout:  15 * time.Second,
+		Addr:    "0.0.0.0:8080",
+		// Good practice to set timeouts to avoid Slowloris attacks.
+		WriteTimeout: time.Second * 15,
+		ReadTimeout:  time.Second * 15,
+		IdleTimeout:  time.Second * 60,
 	}
 
-	err = srv.ListenAndServe()
-	if err != nil {
-		panic(fmt.Errorf("Unable to listen: %s", err))
-	}
+	go func() {
+		if err := srv.ListenAndServe(); err != nil {
+			if errors.Is(http.ErrServerClosed, err) {
+				return
+			}
+			panic(fmt.Errorf("Unable to listen: %s", err))
+		}
+	}()
+
+	c := make(chan os.Signal, 1)
+	// We'll accept graceful shutdowns when quit via SIGINT (Ctrl+C)
+	// SIGKILL, SIGQUIT or SIGTERM (Ctrl+/) will not be caught.
+	signal.Notify(c, os.Interrupt)
+
+	// Block until we receive our signal.
+	<-c
+
+	// Create a deadline to wait for.
+	ctx, cancel := context.WithTimeout(context.Background(), wait)
+	defer cancel()
+	// Doesn't block if no connections, but will otherwise wait
+	// until the timeout deadline.
+	srv.Shutdown(ctx)
+	// Optionally, you could run srv.Shutdown in a goroutine and block on
+	// <-ctx.Done() if your application should wait for other services
+	// to finalize based on context cancellation.
+	log.Println("shutting down")
+	os.Exit(0)
+
 }
